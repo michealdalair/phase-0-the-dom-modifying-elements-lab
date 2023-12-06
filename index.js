3916564154134cfc25259763fc919033a8576a7a
michaljones <micheal.jones2010@yahoo.com>
2023-12-05 22:13:14 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+
+const element = document.getElementById("main");
+element.remove(); 
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory",)
+newHeader.textContent= "Micheal is the champion"
+
+
+
